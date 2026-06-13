747b1136911ee0cfe44e2e0bdf024537e5ae67d3
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-06-13 23:51:04 +0500

    refactor: simplify webpack configuration and update dev script

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    webpack: (config) => {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-      return config;
-    },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
